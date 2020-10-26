12a693dfd6212501b9f672d624e403060261e581
gandhiparth16 <69316051+gandhiparth16@users.noreply.github.com>
2020-10-26 23:38:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Layout from "../src/components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
+/*export default function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+*/
