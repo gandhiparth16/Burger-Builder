003542cee0b188ef0273bc79ba3c8349406b2988
gandhiparth16 <69316051+gandhiparth16@users.noreply.github.com>
2020-11-06 22:02:37 +0530

    Folder restructure

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Layout.css";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../../src/components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../../src/components/Navigation/SideDrawer/SideDrawer";
 class Layout extends React.Component {
   state = { showSideDrawer: false };
 
